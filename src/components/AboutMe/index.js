5113823649882426e67fbf6689169b44847f8187
Dafa Ardiansyah <ardidafa21@gmail.com>
2024-09-27 15:39:17 +0700

    Fix Image Bug #2

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -1,11 +1,6 @@
-import React from 'react';
-import HeroBgAnimation from '../HeroBgAnimation';
-import {
-    HeroContainer, HeroBg, HeroLeftContainer, Img,
-    HeroRightContainer, HeroInnerContainer, TextLoop,
-    Title, Span, SubTitle, SocialMediaIcons, SocialMediaIcon,
-    ResumeButton, ButtonContainer
-} from './AboutmeStyle';
+import React from 'react'
+import HeroBgAnimation from '../HeroBgAnimation'
+import { HeroContainer, HeroBg, HeroLeftContainer, Img, HeroRightContainer, HeroInnerContainer, TextLoop, Title, Span, SubTitle,SocialMediaIcons,SocialMediaIcon, ResumeButton, ButtonContainer } from './AboutmeStyle'
 import Typewriter from 'typewriter-effect';
 import { Bio } from '../../data/constants';
 
@@ -18,19 +13,27 @@ const AboutMe = () => {
                 </HeroBg>
                 <HeroInnerContainer >
                     <HeroLeftContainer id="Left">
-                        {/* ... (rest of your left container code) */}
+                        <Title>About Me</Title>
+                        <SubTitle>{Bio.description}</SubTitle>
+                        <ButtonContainer>
+                            <ResumeButton href={Bio.linkedin} target='display'>LinkedIn</ResumeButton>
+                            <ResumeButton href={Bio.github} target='display'>Github</ResumeButton>
+                            <ResumeButton href={Bio.email} target='display'>Email</ResumeButton>
+                        </ButtonContainer>
                     </HeroLeftContainer>
 
                     <HeroRightContainer id="Right">
+
                         <Img
                             src="https://firebasestorage.googleapis.com/v0/b/portfolioweb-b5005.appspot.com/o/PhotoHero%2FHeroImages2.png?alt=media&token=15b1aa4d-cc2c-4a2c-81ed-97b5afc9170a"
                             alt="hero-image2"
                         />
                     </HeroRightContainer>
                 </HeroInnerContainer>
+
             </HeroContainer>
         </div>
-    );
-};
+    )
+}
 
-export default AboutMe;
+export default AboutMe
