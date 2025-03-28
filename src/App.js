1249497f0ab87d9d5ca0ee489e87e31548885010
Dafa Ardiansyah <ardidafa21@gmail.com>
2025-03-28 11:53:40 +0700

    fix(ci/cd): fix react 17 problem #6

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { ThemeProvider } from "styled-components";
 import { useState, useEffect } from "react";
 import { darkTheme, lightTheme } from './utils/Themes.js'
